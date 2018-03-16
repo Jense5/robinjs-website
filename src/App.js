3221449c2ba3dde05615404d3f52d54d6de24f66
Jensen Bernard <jensenbernard5@gmail.com>
2018-03-16 19:51:42 +0100

    Finished site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { h, Component } from 'preact';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="Content">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Preact</h2>
+          <h2>Hi, I am Robin!</h2>
+          <p className="Subtitle">
+            Start your sentence with "Robin", and I will reply.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
