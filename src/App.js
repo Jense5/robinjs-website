c5a999e89ac6cdeae22cb32dcd11ea0077ec3cc0
Jensen Bernard <jensenbernard5@gmail.com>
2018-03-16 20:04:29 +0100

    Add alert

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import { h, Component } from 'preact';
 import { detect } from 'detect-browser';
 import logo from './logo.svg';
+import alert from './alert.svg';
 
 const browser = detect();
 
 class App extends Component {
   render() {
-    if (!browser || browser.name !== 'chrome') 
-      return <div>Woops, you need chrome to run this app!</div>;
+    const icon = (!browser || browser.name !== 'chrome') ? alert : logo;
     return (
       <div className="App">
         <div className="Content">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hi, I am Robin!</h2>
+          <img src={icon} className="App-logo" alt="logo" />
+          <h2>
+            { (!browser || browser.name !== 'chrome')
+              ? 'Uh - Oh'
+              : 'Hi, I am Robin!'
+            }
+          </h2>
           <p className="Subtitle">
-            Start your sentence with "Robin", and I will reply.
+            { (!browser || browser.name !== 'chrome')
+              ? 'You need Chrome to use this website!'
+              : 'Start your sentence with "Robin", and I will reply.'
+            }
           </p>
         </div>
       </div>
