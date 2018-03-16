9669bab9cbad961ef420c15c16e9de8e40765888
Jensen Bernard <jensenbernard5@gmail.com>
2018-03-16 19:43:30 +0100

    Add the listener

--- src/assistant.js
+++ src/assistant.js
@@ -8,6 +8,24 @@ const defaults = {
 export default class Assistant {
   constructor(config = {}) {
     this.config = { ...defaults, ...config };
+    this.recognizer = new webkitSpeechRecognition();
+    this.recognizer.lang = this.config.language;
+    this.recognizer.addEventListener('end', () => {
+      this.recognizer.start();
+    });
+    this.recognizer.addEventListener('result', (instance) => {
+      const transcript = Array.from(instance.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .map(str => str.toLowerCase())[0];
+      const words = transcript.split(' ');
+      if (words[0] === this.config.name.toLocaleLowerCase()) {
+        this.process(words.slice(1).join(' '));
+      }
+    });
+  }
+  start() {
+    this.recognizer.start();
   }
   process(sentence) {
     for (const skill of this.config.skills)
