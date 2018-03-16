eca6a217df4e76a33ff2bc6ccd15cc85708f7ce6
Jensen Bernard <jensenbernard5@gmail.com>
2018-03-16 19:44:49 +0100

    Started listener on boot

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import './index.css';
 import Assistant from './assistant';
 
 const assistant = new Assistant({
-  name: 'Alexa',
   skills: [
     require('./skills/time'),
     require('./skills/whatsup'), 
   ],
 });
 
-assistant.process('turn on the heating');
-assistant.process('how are you');
-assistant.process('what time is it');
+assistant.start();
 
 render(
   <App />,
